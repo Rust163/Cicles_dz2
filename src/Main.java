4f8c82e6577065579392f8ec14163f6e602724e0
Рустам <rustam19871203@gmail.com>
2022-10-12 14:23:45 +0400

    Циклы часть 1. Домашка 2 задачи с 1-3

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+//Задача 1
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 1904; i <= 2096; i = i + 4)
+            System.out.println(i + " Високосный год");
+
+        //Задача 2
+        for (int i = 7; i <= 98; i = i + 7)
+            System.out.print(i + " ");
+        System.out.println();
+
+        //Задача 3
+        for (int i = 1; i <= 512; i = i * 2)
+            System.out.print(i + " ");
+
+    }
+}
